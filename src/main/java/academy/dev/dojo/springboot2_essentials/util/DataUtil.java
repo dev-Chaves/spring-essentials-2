34e665f599661781410afbde4de97fd317159531
chaves <chavesprogrammer@gmail.com>
2025-01-27 12:53:36 -0300

    formatador de data

--- src/main/java/academy/dev/dojo/springboot2_essentials/util/DataUtil.java
+++ src/main/java/academy/dev/dojo/springboot2_essentials/util/DataUtil.java
@@ -1,4 +1,16 @@
 package academy.dev.dojo.springboot2_essentials.util;
 
+import org.springframework.cglib.core.Local;
+import org.springframework.stereotype.Component;
+
+import javax.swing.text.DateFormatter;
+import javax.xml.crypto.Data;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
 public class DataUtil {
+   public String formatLocalDataTimeToDatabaseStyle(LocalDateTime localDateTime){
+       return DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime);
+   }
 }
