ff537d54404be371b66c72e80eb93203142685fb
chaves <chavesprogrammer@gmail.com>
2025-01-28 12:55:24 -0300

    adição de construtores na classe anime java, para leitura, remoÃção do lombok

--- src/main/java/academy/dev/dojo/springboot2_essentials/domain/Anime.java
+++ src/main/java/academy/dev/dojo/springboot2_essentials/domain/Anime.java
@@ -1,12 +1,27 @@
 package academy.dev.dojo.springboot2_essentials.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-@Data
-@AllArgsConstructor
-
 public class Anime {
     private String name;
     private long id;
+
+    public Anime(String name, long id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
