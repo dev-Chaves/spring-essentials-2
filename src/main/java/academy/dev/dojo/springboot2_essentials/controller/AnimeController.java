d613cb4f16235248f415be6f282dac62dd61fcce
chaves <chavesprogrammer@gmail.com>
2025-01-28 12:56:37 -0300

    adição da injeção de dependencias usando construtores/funcionando

--- src/main/java/academy/dev/dojo/springboot2_essentials/controller/AnimeController.java
+++ src/main/java/academy/dev/dojo/springboot2_essentials/controller/AnimeController.java
@@ -10,19 +10,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("animes")
 @Log4j2
-@RequiredArgsConstructor
 public class AnimeController {
 
-//    private final DataUtil dateUtil;
+    private final DataUtil dateUtil;
 
     private final AnimeService animeService;
 
     // fazer injeção de dependencia
+
+    public AnimeController(DataUtil dateUtil, AnimeService animeService) {
+        this.dateUtil = dateUtil;
+        this.animeService = animeService;
+    }
 
     @GetMapping()
     public List<Anime> list(){
