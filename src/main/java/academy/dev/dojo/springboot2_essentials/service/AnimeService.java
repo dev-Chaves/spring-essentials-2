6b3f41590d9cacd35be4ab57599139e1b2c9d33d
chaves <chavesprogrammer@gmail.com>
2025-01-29 14:24:50 -0300

    método pasave/gera um id aleatÂório e add um anime na lista anime

--- src/main/java/academy/dev/dojo/springboot2_essentials/service/AnimeService.java
+++ src/main/java/academy/dev/dojo/springboot2_essentials/service/AnimeService.java
@@ -6,14 +6,20 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.stream.Collectors;
 
 @Service
 public class AnimeService {
 
-    private List<Anime> animes = List.of(new Anime("Berserk", 1L), new Anime("Monster", 2L));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime("Berserk", 1L), new Anime("Monster", 2L)));
+    }
 
     public List<Anime> listAll(){
         return animes;
@@ -24,4 +30,12 @@ public class AnimeService {
                 .filter(anime -> (long) anime.getId() == id)
                 .findFirst().orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,"Anime not Found"));
         };
+
+    public Anime save(Anime anime){
+
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 1000));
+        animes.add(anime);
+        return anime;
+    }
+
     }
